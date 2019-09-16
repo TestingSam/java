2806f6914db0ae17b6112c7f116f6c18fc98f6bc
TestingSam <samiralurhp@gmail.com>
2019-09-16 09:14:27 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,4 @@
+Passport -->https://www.codeproject.com/Questions/1046445/How-to-validate-passport-number-using-javascript
+SSN-->https://www.ssnvalidator.com/index.aspx
+Address Validator==>https://rapidapi.com/byteplant/api/byteplant-postal-address-online-validator
+File upload: https://programmingwithmosh.com/javascript/react-file-upload-proper-server-side-nodejs-easy/
